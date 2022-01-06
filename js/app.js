3fe2a8ba703eef97d1986c97eca21775d7f21290
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2022-01-06 17:42:49 -0600

    generamos el objeto de gasto

--- js/app.js
+++ js/app.js
@@ -2,15 +2,14 @@
 const formulario = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
 
-let presupuesto;
-const ui = new UI();
-
 
 // Eventos
 
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 
@@ -24,8 +23,44 @@ class Presupuesto {
 }
 
 class UI {
+    insertarPresupuesto(cantidad) {
+        // Extraemos el valor
+        const { presupuesto, restante } = cantidad;
+
+        // Agregamos a el HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo) {
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Agregamos el mensaje
+        divMensaje.textContent = mensaje;
+
+        // lo insertamos
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        },3000);
+    }
 
 }
+
+//Inicalizaciones
+
+let presupuesto;
+const ui = new UI();
+
 
 // Funciones
 
@@ -37,5 +72,29 @@ function preguntarPresupuesto() {
     }
 
     presupuesto = new Presupuesto(presupuestoUsuario);
-    console.log(presupuesto);
+    ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+    e.preventDefault();
+
+    // leer los datos
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number(document.querySelector('#cantidad').value);
+
+    // Validar
+    if(nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('Ambos campos son obligatorios','error');
+
+        return;
+    } else if(cantidad <= 0 || isNaN(cantidad)) {
+        ui.imprimirAlerta('Cantidad no valida','error');
+
+        return;
+    }
+
+    // Generamos el objeto de gasto
+    const gasto = { nombre, cantidad, id: Date.now() };
+    
+    console.log(gasto);
 }
